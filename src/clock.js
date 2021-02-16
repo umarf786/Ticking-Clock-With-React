456e94a9de8b23ae1948b04553868a088111b51b
Umar Farooq <admin@DESKTOP-CUU7BND-4.local>
2021-02-16 11:37:40 +0000

    finished

--- src/clock.js
+++ src/clock.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 import Footer from './footer';
 
 import './clock.css';
+var d = new Date();
+d = new Date(d.setHours(d.getHours() + 2));
+
 class Clock extends Component {
     constructor(props){
         super(props);
 
         //This declared the state of time at the very beginning
         this.state ={
-            time: new Date().toLocaleTimeString()
+            time: new Date().toLocaleTimeString(),
+            newtime: d.toLocaleTimeString()
         }
         
     }
@@ -29,16 +33,21 @@ class Clock extends Component {
 
     //This function set the state of the time to a new time
     updateClock(){
+        var d = new Date();
+        d = new Date(d.setHours(d.getHours() + 2));
         this.setState({
-            time: new Date().toLocaleTimeString()
+            time: new Date().toLocaleTimeString(),
+            newtime: d.toLocaleTimeString()
         });
+        
+        
     }
 
     render() {
         return (
             <div className="Time">
-                <p> {this.state.time}</p>
-                <Footer/>
+                <p> {this.state.newtime}</p>
+                <Footer actual={this.state.time}/>
             </div>
             
         );
--- src/footer.js
+++ src/footer.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './footer.css';
 
-const Footer = () => {
-    return (
-        <div className="footer">
-            <h2>View Code: <a href="https://github.com/Olanetsoft/Ticking-Clock-With-React" target="_blank">Click Here</a></h2>
-        </div>
-    );
-};
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <h2>YST: Youness Standard Time <br></br> (+2hr your local time)</h2>
+                <h3>The actual time is {this.props.actual}</h3>
+            </div>
+        );
+    }
+  }
+  
 
 export default Footer;
